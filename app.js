c98ac367c5b3284a665f3943ae9e0f03201ce72e
Amin <amin@araddata.com>
2022-06-28 14:04:56 +0430

    Small bug fix in setting timezone

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ app.post('/update_schedule', async(req, res)=>{
 app.get('/faucet_stat', async(req, res) => {
   try {
     const result = await get_faucet_stat();
-    const now = new Date().toLocaleString({timeZone: 'Asia/Tehran'});
+    const now = new Date().toLocaleString('en-US', {timeZone: 'Asia/Tehran'});
     let response = {1: false, 2: false};
     result.forEach(i => {
-      const from = new Date(`${i.s_date} ${i.s_from}`).toLocaleString({timeZone: 'Asia/Tehran'});
-      const to = new Date(`${i.s_date} ${i.s_to}`).toLocaleString({timeZone: 'Asia/Tehran'});
+      const from = new Date(`${i.s_date} ${i.s_from}`).toLocaleString('en-US', {timeZone: 'Asia/Tehran'});
+      const to = new Date(`${i.s_date} ${i.s_to}`).toLocaleString('en-US', {timeZone: 'Asia/Tehran'});
       if(from <= now && now <= to) {
         response[i.faucet_id] = true;
       }
