111b3bb9480a1b43bdfa3f2cbee722303b81af66
Amin <amin@araddata.com>
2022-06-30 20:55:30 +0430

    Add is_ok to /status & Bugfix

--- app.js
+++ app.js
@@ -13,12 +13,30 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(morgan('combined'));
 
+const faucet_schedule_stat = async() => {
+  const result = await get_faucet_stat();
+  const now = new Date().toLocaleString('en-US', {timeZone: 'Asia/Tehran'});
+  let response = {1: false, 2: false};
+  result.forEach(i => {
+    const from = new Date(`${i.s_date} ${i.s_from}`).toLocaleString('en-US');
+    const to = new Date(`${i.s_date} ${i.s_to}`).toLocaleString('en-US');
+    if(new Date(from) <= new Date(now) && new Date(now) <= new Date(to)) {
+      response[i.faucet_id] = true;
+    }
+  })
+  return response;
+};
+
 //Frontend side endpoints
 app.get('/status', async(req, res) => {
   try{
     const result = await get_status();
+    const schedule_stat = await faucet_schedule_stat();
     const response = {};
-    result[1].forEach(i => response[i.faucet_id] = {is_on: i.is_on, schedule: []});
+    result[1].forEach(i => {
+      const is_ok = i.is_on == schedule_stat[i.faucet_id];
+      response[i.faucet_id] = {is_on: i.is_on, is_ok, schedule: []}
+    });
     result[0].forEach(i => {
       response[i.faucet_id].schedule.push({s_from: i.s_from, s_to: i.s_to})
     });
@@ -44,16 +62,7 @@ app.post('/update_schedule', async(req, res)=>{
 //Hardware side endpoints
 app.get('/faucet_stat', async(req, res) => {
   try {
-    const result = await get_faucet_stat();
-    const now = new Date().toLocaleString('en-US', {timeZone: 'Asia/Tehran'});
-    let response = {1: false, 2: false};
-    result.forEach(i => {
-      const from = new Date(`${i.s_date} ${i.s_from}`).toLocaleString('en-US');
-      const to = new Date(`${i.s_date} ${i.s_to}`).toLocaleString('en-US');
-      if(from <= now && now <= to) {
-        response[i.faucet_id] = true;
-      }
-    })
+    const response = await faucet_schedule_stat();
     res.json(response);
   } catch (error) {
     console.log(`[ERROR] /faucet_stat endpoint #$# ${error.message} #$#`);
